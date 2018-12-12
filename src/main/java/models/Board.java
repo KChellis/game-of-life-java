90e5afbb7ba03d5fe9feabd3c802e3bc9ddfbf9a
Kayl Eubanks <kayleubanks@gmail.com>
2018-12-11 19:05:41 -0800

    Change variable to boardSize from xaxis and add getter function for boardSize

--- src/main/java/models/Board.java
+++ src/main/java/models/Board.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Board {
-    private int xaxis;
+    private int boardSize;
 
     public Board(){
         this.boardSetup();
@@ -16,7 +16,7 @@ public class Board {
         try {
             System.out.println("Welcome to the Game of Life!");
             System.out.println("Please enter the size for your board (1-10): ");
-            xaxis = Integer.parseInt(bufferedReader.readLine());
+            boardSize = Integer.parseInt(bufferedReader.readLine());
             System.out.println("Your board size is " + xaxis + " columns by " + xaxis + " rows.");
         } catch(IOException e) {
             e.printStackTrace();
@@ -25,12 +25,16 @@ public class Board {
 
     public void printBoard() {
         String boardRow = "";
-        for (int z = 0; z < xaxis; z++) {
+        for (int z = 0; z < boardSize; z++) {
             boardRow = boardRow + "_ ";
         }
         System.out.println("The board: ");
-        for (int z = 0; z < xaxis; z++) {
+        for (int z = 0; z < boardSize; z++) {
             System.out.println(boardRow);
         }
+    }
+
+    public int getBoardSize() {
+        return boardSize;
     }
 }
